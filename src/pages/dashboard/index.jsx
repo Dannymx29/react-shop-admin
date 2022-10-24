b90b26121bec1ac0c5d100a8fd4f55a7763f7273
DannyMX29 <bran.dan.her.gar@gmail.com>
2022-10-24 17:43:32 -0500

    obteniendo lista de productos desde la api

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -12,8 +12,8 @@ const people = [
 import endPoints from "@services/api";
 import useFetch from "@hook/useFetch";
 
-const PRODUCT_LIMIT = 60;
-const PRODUCT_OFFSET = 60;
+const PRODUCT_LIMIT = 5;
+const PRODUCT_OFFSET = 5;
 
 export default function Dashboard() {
   const products = useFetch(endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
@@ -27,22 +27,25 @@ export default function Dashboard() {
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="relative px-5 py-3">
+                      <span className="sr-only">Images</span>
+                    </th>
+                    <th scope="col" className="px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Name
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Category
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Price
                     </th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="px-5 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                       Id
                     </th>
-                    <th scope="col" className="relative px-6 py-3">
+                    <th scope="col" className="relative px-5 py-3">
                       <span className="sr-only">Edit</span>
                     </th>
-                    <th scope="col" className="relative px-6 py-3">
+                    <th scope="col" className="relative px-5 py-3">
                       <span className="sr-only">Delete</span>
                     </th>
                   </tr>
@@ -53,11 +56,13 @@ export default function Dashboard() {
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
-                            <img className="h-10 w-10 rounded-full" src={product.images[0]} alt={product.title} />
+                            <img className="h-10 w-10 rounded-full" src={product.images[1]} alt={product.title} />
                           </div>
-                          <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{product.title}</div>
-                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">{product.title}</div>
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
